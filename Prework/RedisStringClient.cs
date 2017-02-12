9db57e6dc51768d6b6eda51c38459082de634ed9
Thomas Ackling <thomas.ackling@smsmt.com>
2017-02-12 17:04:00 +1030

    adding mocking prework

---
+++ Prework/RedisStringClient.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prework
+{
+    public class RedisStringClient
+    {
+        public const string StringChangeChannel = "string-change";
+        private readonly IDatabase _redisDatabase;
+
+        public RedisStringClient(IDatabase redisDatabase)
+        {
+            _redisDatabase = redisDatabase;
+        }
+
+        public void Set(string key, string value)
+        {
+            _redisDatabase.StringSet(key, value);
+            _redisDatabase.Publish(StringChangeChannel, key+":"+value);
+        }
+
+        public void GetValue(string key)
+        {
+            _redisDatabase.StringGet(key);
+        }
+    }
+}
---
+++ PreworkTests/RedisStringClientTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PreworkTests
+{
+    [TestClass]
+    public class RedisStringClientTest
+    {
+        [TestMethod]
+        public void TestSetterPublishesToServer()
+        {
+            // TODO: Test that the set function publishes a change on the string change channel using NSubstitute.
+        }
+    }
+}
